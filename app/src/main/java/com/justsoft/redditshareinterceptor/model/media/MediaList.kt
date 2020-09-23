b31caf727decac084600ac4ab8c9c5fe8004f8cd
Max <rudko.max10@gmail.com>
2020-09-23 14:51:54 +0300

    Prepare for adaptable media downloads

---
+++ app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaList.kt
@@ -0,0 +1,68 @@
+package com.justsoft.redditshareinterceptor.model.media
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import java.util.function.UnaryOperator
+
+class MediaList(private val listMediaContentType: MediaContentType) : ArrayList<MediaModel>() {
+
+    private fun checkElementContentType(element: MediaModel) {
+        if (element.mediaType != listMediaContentType)
+            throw IllegalArgumentException(
+                "Can't add element to list because it has different type: " +
+                        "List has type $listMediaContentType while the element has typ ${element.mediaType}"
+            )
+    }
+
+    @RequiresApi(Build.VERSION_CODES.N)
+    override fun replaceAll(operator: UnaryOperator<MediaModel>) {
+        super.replaceAll(operator)
+        this.forEach(::checkElementContentType)
+    }
+
+    override fun set(index: Int, element: MediaModel): MediaModel {
+        checkElementContentType(element)
+        return super.set(index, element)
+    }
+
+    override fun addAll(index: Int, elements: Collection<MediaModel>): Boolean {
+        elements.forEach(::checkElementContentType)
+        return super.addAll(index, elements)
+    }
+
+    override fun addAll(elements: Collection<MediaModel>): Boolean {
+        elements.forEach(::checkElementContentType)
+        return super.addAll(elements)
+    }
+
+    override fun add(index: Int, element: MediaModel) {
+        checkElementContentType(element)
+        super.add(index, element)
+    }
+
+    override fun add(element: MediaModel): Boolean {
+        checkElementContentType(element)
+        return super.add(element)
+    }
+
+    private fun processSubList(mediaSpec: MediaSpec, sortedList: List<MediaModel>): MediaModel {
+        val threshold = mediaSpec.getThresholdForType(listMediaContentType)
+        val iterator = sortedList.iterator()
+        while (iterator.hasNext()) {
+            val file = iterator.next()
+            if (file.size <= threshold)
+                return file
+        }
+        return sortedList.last()
+    }
+
+    fun getMostSuitableMedia(mediaSpec: MediaSpec = MediaSpec()): List<MediaModel> {
+        val sortedList = this.sortedWith(compareBy(MediaModel::index, { -it.size }))
+        return sortedList
+            .groupBy(MediaModel::index)
+            .map { processSubList(mediaSpec, it.value) }
+    }
+}
+
+fun mediaListOf(contentType: MediaContentType, vararg media: MediaModel): MediaList =
+    MediaList(contentType).apply { this.addAll(media) }
---
+++ app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaSpec.kt
@@ -0,0 +1,27 @@
+package com.justsoft.redditshareinterceptor.model.media
+
+class MediaSpec(
+    videoSizeThreshold: Long = RECOMMENDED_VIDEO_SIZE_THRESHOLD,
+    gifSizeThreshold: Long = RECOMMENDED_GIF_SIZE_THRESHOLD,
+    imageSizeThreshold: Long = RECOMMENDED_IMAGE_SIZE_THRESHOLD,
+    galleryImageSizeThreshold: Long = RECOMMENDED_IMAGE_SIZE_THRESHOLD
+) {
+    private val specMap: Map<MediaContentType, Long> = mapOf(
+        MediaContentType.VIDEO to videoSizeThreshold,
+        MediaContentType.GIF to gifSizeThreshold,
+        MediaContentType.IMAGE to imageSizeThreshold,
+        MediaContentType.GALLERY to galleryImageSizeThreshold,
+    )
+
+    fun getThresholdForType(mediaContentType: MediaContentType): Long {
+        if (mediaContentType == MediaContentType.TEXT)
+            throw IllegalArgumentException("There are no specs for text as it")
+        return specMap[mediaContentType] ?: error("No such key: $mediaContentType")
+    }
+
+    companion object {
+        private const val RECOMMENDED_VIDEO_SIZE_THRESHOLD = (10 * 1024 * 1024).toLong()    // 10 MiB
+        private const val RECOMMENDED_GIF_SIZE_THRESHOLD = (10 * 1024 * 1024).toLong()      // 10 MiB
+        private const val RECOMMENDED_IMAGE_SIZE_THRESHOLD = (1.5 * 1024 * 1024).toLong()   // 1.5 MiB
+    }
+}
