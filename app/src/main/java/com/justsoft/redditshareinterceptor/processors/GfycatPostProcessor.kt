33fd17dbb17fa9b2db0aed58da21fd9f8e97487d
Max <rudko.max10@gmail.com>
2020-09-23 19:00:29 +0300

    Update GfycatPostProcessor.kt to use MediaModels

--- app/src/main/java/com/justsoft/redditshareinterceptor/processors/GfycatPostProcessor.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/processors/GfycatPostProcessor.kt
@@ -1,11 +1,11 @@
 package com.justsoft.redditshareinterceptor.processors
 
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import com.justsoft.redditshareinterceptor.model.RedditPost
-import com.justsoft.redditshareinterceptor.model.media.MediaContentType
-import com.justsoft.redditshareinterceptor.model.media.MediaList
+import com.justsoft.redditshareinterceptor.model.media.*
 import com.justsoft.redditshareinterceptor.util.RequestHelper
-import org.jsoup.Jsoup
+import java.util.regex.Pattern
 
 class GfycatPostProcessor : PostProcessor {
     override fun isProcessorSuitableForPost(redditPost: RedditPost): Boolean =
@@ -17,16 +17,53 @@ class GfycatPostProcessor : PostProcessor {
         requestHelper: RequestHelper
     ): MediaContentType = MediaContentType.VIDEO
 
-    override fun getAllPossibleMediaDownloads(
+    override fun downloadMediaMatchingMediaSpec(
         redditPost: RedditPost,
         savedState: Bundle,
-        requestHelper: RequestHelper
+        requestHelper: RequestHelper,
+        mediaSpec: MediaSpec,
+        destinationDescriptorGenerator: (MediaContentType, Int) -> ParcelFileDescriptor
     ): MediaList {
-        val htmlDoc = Jsoup.connect(
-            redditPost.url
-        ).get()
-        val sourceElement = htmlDoc
-            .getElementsByAttributeValueMatching("src", "https://giant.gfycat.com/\\w*.mp4")[0]
-        return sourceElement.attr("src")
+        val gfycatResponse = requestHelper
+            .readHttpJsonResponse("$GFYCAT_API_GIF_REQUEST${getGfycatId(redditPost.url)}")
+        val contentUrls = gfycatResponse
+            .getJSONObject("gfyItem")
+            .getJSONObject("content_urls")
+
+        val allMediaList = mediaListOf(MediaContentType.VIDEO)
+
+        val keysIterator = contentUrls.keys()
+        while (keysIterator.hasNext()) {
+            val formatType = keysIterator.next()
+            if (formatType == "webp" || formatType == "webm" || formatType == "mobilePoster")
+                continue
+
+            val mediaObj = contentUrls.getJSONObject(formatType)
+
+            allMediaList.add(
+                MediaModel(
+                    mediaObj.getString("url"),
+                    mediaObj.getLong("size"),
+                    MediaContentType.VIDEO
+                )
+            )
+        }
+
+        val bestMedia = allMediaList.getMostSuitableMedia(mediaSpec)
+        requestHelper.downloadFile(
+            bestMedia[0].downloadUrl,
+            destinationDescriptorGenerator(MediaContentType.VIDEO, 0)
+        )
+        return bestMedia
+    }
+
+    private fun getGfycatId(sourceUrl: String): String {
+        val matcher = Pattern.compile("com/([a-zA-Z0-9].+)").matcher(sourceUrl)
+        matcher.find()
+        return matcher.group(1)!!
+    }
+
+    companion object {
+        private const val GFYCAT_API_GIF_REQUEST = "https://api.gfycat.com/v1/gfycats/"
     }
 }
