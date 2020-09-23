9d0b2848001b936bb6ef84879e0c8d7a1c70c171
Max <rudko.max10@gmail.com>
2020-09-23 16:08:45 +0300

    Update to use MediaModels

--- app/src/main/java/com/justsoft/redditshareinterceptor/processors/TextPostProcessor.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/processors/TextPostProcessor.kt
@@ -1,11 +1,9 @@
 package com.justsoft.redditshareinterceptor.processors
 
 import android.os.Bundle
+import android.os.ParcelFileDescriptor
 import com.justsoft.redditshareinterceptor.model.RedditPost
-import com.justsoft.redditshareinterceptor.model.media.MediaContentType
-import com.justsoft.redditshareinterceptor.model.media.MediaList
-import com.justsoft.redditshareinterceptor.model.media.MediaModel
-import com.justsoft.redditshareinterceptor.model.media.mediaListOf
+import com.justsoft.redditshareinterceptor.model.media.*
 import com.justsoft.redditshareinterceptor.util.RequestHelper
 
 class TextPostProcessor : PostProcessor {
@@ -19,9 +17,11 @@ class TextPostProcessor : PostProcessor {
         requestHelper: RequestHelper
     ): MediaContentType = MediaContentType.TEXT
 
-    override fun getAllPossibleMediaDownloads(
+    override fun downloadMediaMatchingMediaSpec(
         redditPost: RedditPost,
         savedState: Bundle,
-        requestHelper: RequestHelper
-    ): MediaList = mediaListOf(MediaContentType.TEXT, MediaModel(redditPost.url, 0, MediaContentType.TEXT))
+        requestHelper: RequestHelper,
+        mediaSpec: MediaSpec,
+        destinationDescriptorGenerator: (MediaContentType, Int) -> ParcelFileDescriptor
+    ): MediaList = mediaListOf(MediaModel(redditPost.url, 0, MediaContentType.TEXT))
 }
