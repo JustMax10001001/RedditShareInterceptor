e01e97d412f95dd540472d3b3b90daa672b848c9
Max <rudko.max10@gmail.com>
2020-09-26 01:47:37 +0300

    Add progress support

--- app/src/main/java/com/justsoft/redditshareinterceptor/downloaders/ImageDownloader.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/downloaders/ImageDownloader.kt
@@ -7,7 +7,7 @@ import com.justsoft.redditshareinterceptor.model.media.MediaList
 import com.justsoft.redditshareinterceptor.util.RequestHelper
 import java.io.OutputStream
 
-class ImageDownloader: MediaDownloader {
+class ImageDownloader : MediaDownloader {
     override fun downloadMedia(
         mediaList: MediaList,
         requestHelper: RequestHelper,
@@ -21,7 +21,8 @@ class ImageDownloader: MediaDownloader {
             mediaList[0].downloadUrl,
             outputStreamCallback(uri)
         ) { processingProgress ->
-            val progress = (processingProgress.overallProgress.toDouble() / mediaList[0].size * 100).toInt()
+            val progress =
+                (processingProgress.overallProgress.toDouble() / mediaList[0].size * 100).toInt()
             if (progress != prevProgress) {
                 prevProgress = progress
                 downloadProgressCallback(ProcessingProgress(-1, progress))
--- app/src/main/java/com/justsoft/redditshareinterceptor/downloaders/VideoDownloader.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/downloaders/VideoDownloader.kt
@@ -7,7 +7,7 @@ import com.justsoft.redditshareinterceptor.model.media.MediaList
 import com.justsoft.redditshareinterceptor.util.RequestHelper
 import java.io.OutputStream
 
-class VideoDownloader: MediaDownloader {
+class VideoDownloader : MediaDownloader {
     override fun downloadMedia(
         mediaList: MediaList,
         requestHelper: RequestHelper,
@@ -16,10 +16,18 @@ class VideoDownloader: MediaDownloader {
         downloadProgressCallback: (ProcessingProgress) -> Unit
     ): List<Uri> {
         val uri = destinationUriCallback(MediaContentType.VIDEO, 0)
+        var prevProgress = 0
         requestHelper.downloadToOutputStream(
             mediaList[0].downloadUrl,
             outputStreamCallback(uri)
-        )
+        ) { processingProgress ->
+            val progress =
+                (processingProgress.overallProgress.toDouble() / mediaList[0].size * 100).toInt()
+            if (progress != prevProgress) {
+                prevProgress = progress
+                downloadProgressCallback(ProcessingProgress(-1, progress))
+            }
+        }
         return listOf(uri)
     }
 }
