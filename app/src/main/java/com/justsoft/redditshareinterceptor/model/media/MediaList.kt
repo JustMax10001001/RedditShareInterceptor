f242ce86ce844879a03fc0177bd2a3f24e3ad892
Max <rudko.max10@gmail.com>
2020-09-26 02:18:09 +0300

    Clean up

--- app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaList.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaList.kt
@@ -2,44 +2,6 @@ package com.justsoft.redditshareinterceptor.model.media
 
 class MediaList(val listMediaContentType: MediaContentType, var caption: String = "") : ArrayList<MediaModel>() {
 
-    /*private fun checkElementContentType(element: MediaModel) {
-        if (element.mediaType != listMediaContentType)
-            throw IllegalArgumentException(
-                "Can't add element to list because it has different type: " +
-                        "List has type $listMediaContentType while the element has typ ${element.mediaType}"
-            )
-    }
-
-    override fun replaceAll(operator: UnaryOperator<MediaModel>) {
-        super.replaceAll(operator)
-        this.forEach(::checkElementContentType)
-    }
-
-    override fun set(index: Int, element: MediaModel): MediaModel {
-        checkElementContentType(element)
-        return super.set(index, element)
-    }
-
-    override fun addAll(index: Int, elements: Collection<MediaModel>): Boolean {
-        elements.forEach(::checkElementContentType)
-        return super.addAll(index, elements)
-    }
-
-    override fun addAll(elements: Collection<MediaModel>): Boolean {
-        elements.forEach(::checkElementContentType)
-        return super.addAll(elements)
-    }
-
-    override fun add(index: Int, element: MediaModel) {
-        checkElementContentType(element)
-        super.add(index, element)
-    }
-
-    override fun add(element: MediaModel): Boolean {
-        checkElementContentType(element)
-        return super.add(element)
-    }*/
-
     private fun processSubList(mediaSpec: MediaSpec, sortedList: List<MediaModel>): MediaModel {
         val threshold = mediaSpec.getThresholdForType(listMediaContentType)
         val iterator = sortedList.iterator()
