1792a03be5b2db83dab5ca2d2c246f8b0ed21c1c
Max <rudko.max10@gmail.com>
2020-09-26 01:34:21 +0300

    Add support for progress reporting

--- app/src/main/java/com/justsoft/redditshareinterceptor/downloaders/GalleryDownloader.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/downloaders/GalleryDownloader.kt
@@ -1,6 +1,7 @@
 package com.justsoft.redditshareinterceptor.downloaders
 
 import android.net.Uri
+import com.justsoft.redditshareinterceptor.model.ProcessingProgress
 import com.justsoft.redditshareinterceptor.model.media.MediaContentType
 import com.justsoft.redditshareinterceptor.model.media.MediaList
 import com.justsoft.redditshareinterceptor.util.RequestHelper
@@ -13,7 +14,8 @@ class GalleryDownloader: MediaDownloader {
         mediaList: MediaList,
         requestHelper: RequestHelper,
         destinationUriCallback: (MediaContentType, Int) -> Uri,
-        outputStreamCallback: (Uri) -> OutputStream
+        outputStreamCallback: (Uri) -> OutputStream,
+        downloadProgressCallback: (ProcessingProgress) -> Unit
     ): List<Uri> {
         val uris = mutableListOf<Uri>()
         runBlocking(Dispatchers.IO) {
--- app/src/main/java/com/justsoft/redditshareinterceptor/downloaders/GifDownloader.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/downloaders/GifDownloader.kt
@@ -1,6 +1,7 @@
 package com.justsoft.redditshareinterceptor.downloaders
 
 import android.net.Uri
+import com.justsoft.redditshareinterceptor.model.ProcessingProgress
 import com.justsoft.redditshareinterceptor.model.media.MediaContentType
 import com.justsoft.redditshareinterceptor.model.media.MediaList
 import com.justsoft.redditshareinterceptor.util.RequestHelper
@@ -11,7 +12,8 @@ class GifDownloader : MediaDownloader {
         mediaList: MediaList,
         requestHelper: RequestHelper,
         destinationUriCallback: (MediaContentType, Int) -> Uri,
-        outputStreamCallback: (Uri) -> OutputStream
+        outputStreamCallback: (Uri) -> OutputStream,
+        downloadProgressCallback: (ProcessingProgress) -> Unit
     ): List<Uri> {
         val uri = destinationUriCallback(MediaContentType.VIDEO, 0)
         requestHelper.downloadToOutputStream(
