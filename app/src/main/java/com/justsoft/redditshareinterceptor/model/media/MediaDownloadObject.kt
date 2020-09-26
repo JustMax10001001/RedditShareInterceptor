857a9ca988b56231e67d359ea87c11c0912d4f4c
Max <rudko.max10@gmail.com>
2020-09-26 20:47:57 +0300

    Refactor with MediaMetadata in mind

--- app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaDownloadObject.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaDownloadObject.kt
@@ -5,7 +5,6 @@ import com.justsoft.redditshareinterceptor.model.media.metadata.MediaMetadata
 data class MediaDownloadObject(
     val downloadUrl: String,
     val mediaType: MediaContentType,
-    val size: Long = 0,
-    val galleryIndex: Int = 0,         // used for galleries
+    val galleryIndex: Int = 0,
     val metadata: MediaMetadata = MediaMetadata()
 )
