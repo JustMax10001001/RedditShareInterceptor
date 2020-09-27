8ad056ba07d78117dcf74d99c8843c7b7af91b4b
Max <rudko.max10@gmail.com>
2020-09-27 21:34:05 +0300

    Clean up

--- app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaDownloadInfo.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaDownloadInfo.kt
@@ -3,24 +3,11 @@ package com.justsoft.redditshareinterceptor.model.media
 class MediaDownloadInfo(
     var mediaContentType: MediaContentType,
     var caption: String = "",
+    srcMediaObjectList: List<MediaDownloadObject> = emptyList()
+) {
     val mediaDownloadList: MutableList<MediaDownloadObject> = mutableListOf()
-)
 
-/*fun mediaDownloadListOf(mediaType: MediaContentType, caption: String = ""): MediaDownloadInfo =
-    MediaDownloadInfo(mediaType, caption)
-
-fun mediaDownloadListOf(contentType: MediaContentType): MediaDownloadInfo =
-    mediaDownloadListOf(contentType, "")
-
-fun mediaDownloadListOf(caption: String, vararg media: MediaDownloadObject): MediaDownloadInfo =
-    mediaDownloadListOf(media[0].mediaType, caption).apply { this.addAll(media) }
-
-fun mediaDownloadListOf(mediaInfo: MediaDownloadInfo): MediaDownloadInfo =
-    MediaDownloadInfo(mediaInfo.mediaContentType, mediaInfo.caption).apply {
-        this.addAll(
-            mediaInfo
-        )
+    init {
+        mediaDownloadList.addAll(srcMediaObjectList)
     }
-
-fun mediaDownloadListOf(vararg media: MediaDownloadObject): MediaDownloadInfo =
-    mediaDownloadListOf(media[0].mediaType).apply { this.addAll(media) }*/
+}
