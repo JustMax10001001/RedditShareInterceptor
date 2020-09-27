312c1ccae864e76acb23f678b404fd69684add1d
Max <rudko.max10@gmail.com>
2020-09-27 21:44:50 +0300

    Update to support new API

--- app/src/test/java/com/justsoft/redditshareinterceptor/model/media/MediaDownloadInfoTest.kt
+++ app/src/test/java/com/justsoft/redditshareinterceptor/model/media/MediaDownloadInfoTest.kt
@@ -1,13 +1,15 @@
 package com.justsoft.redditshareinterceptor.model.media
 
+import com.justsoft.redditshareinterceptor.model.media.MediaContentType.GALLERY
+import com.justsoft.redditshareinterceptor.model.media.MediaContentType.VIDEO
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import org.mockito.Mockito
 
-class MediaDownloadListTest {
+class MediaDownloadInfoTest {
 
     private fun mockVideoMedia(size: Long) =
-        MediaDownloadObject("mock://vid.eo", MediaContentType.VIDEO)
+        MediaDownloadObject("mock://vid.eo", VIDEO)
             .apply { metadata.size = size }
 
     private fun mockImagesForGalleryIndex(
@@ -15,7 +17,7 @@ class MediaDownloadListTest {
         sizes: Sequence<Long>
     ): Sequence<MediaDownloadObject> =
         sizes.map {
-            MediaDownloadObject("mock://galle.ry", MediaContentType.GALLERY, index)
+            MediaDownloadObject("mock://galle.ry", GALLERY, index)
                 .apply { metadata.size = it }
         }
 
@@ -25,10 +27,13 @@ class MediaDownloadListTest {
         return idSequence.flatMap { mockImagesForGalleryIndex(it, sizeSequence).take(5) }
     }
 
+    private fun filterMediaInfo(mediaSpec: MediaQualitySpec, mediaInfo: MediaDownloadInfo): List<MediaDownloadObject>
+        = MediaListFilter.filterListByQualitySpec(mediaSpec, mediaInfo.mediaDownloadList, mediaInfo.mediaContentType)
+
     @Test
     fun getMostSuitableMedia_TestVideo_NormalConditions() {
-        val mediaList = MediaDownloadList(MediaContentType.VIDEO)
-        mediaList.addAll(
+        val mediaList = MediaDownloadInfo(VIDEO)
+        mediaList.mediaDownloadList.addAll(
             listOf(
                 mockVideoMedia(1),
                 mockVideoMedia(2),
@@ -37,13 +42,13 @@ class MediaDownloadListTest {
         )
         val mockSpec = Mockito.mock(MediaQualitySpec::class.java)
         Mockito.`when`(mockSpec.videoFileSize).thenReturn(2)
-        assertEquals(2, mediaList.getMostSuitableMedia(mockSpec)[0].metadata.size)
+        assertEquals(2, filterMediaInfo(mockSpec, mediaList))
     }
 
     @Test
     fun getMostSuitableMedia_TestVideo_ThresholdBiggerThenMaxVideo() {
-        val mediaList = MediaDownloadList(MediaContentType.VIDEO)
-        mediaList.addAll(
+        val mediaList = MediaDownloadInfo(VIDEO)
+        mediaList.mediaDownloadList.addAll(
             listOf(
                 mockVideoMedia(1),
                 mockVideoMedia(2),
@@ -52,13 +57,13 @@ class MediaDownloadListTest {
         )
         val mockSpec = Mockito.mock(MediaQualitySpec::class.java)
         Mockito.`when`(mockSpec.videoFileSize).thenReturn(4)
-        assertEquals(3, mediaList.getMostSuitableMedia(mockSpec)[0].metadata.size)
+        assertEquals(3, filterMediaInfo(mockSpec, mediaList))
     }
 
     @Test
     fun getMostSuitableMedia_TestVideo_ThresholdLowerThenMinVideo() {
-        val mediaList = MediaDownloadList(MediaContentType.VIDEO)
-        mediaList.addAll(
+        val mediaList = MediaDownloadInfo(VIDEO)
+        mediaList.mediaDownloadList.addAll(
             listOf(
                 mockVideoMedia(4),
                 mockVideoMedia(5),
@@ -67,16 +72,16 @@ class MediaDownloadListTest {
         )
         val mockSpec = Mockito.mock(MediaQualitySpec::class.java)
         Mockito.`when`(mockSpec.videoFileSize).thenReturn(2)
-        assertEquals(3, mediaList.getMostSuitableMedia(mockSpec)[0].metadata.size)
+        assertEquals(3, filterMediaInfo(mockSpec, mediaList))
     }
 
     @Test
     fun getMostSuitableMedia_TestGallery_NormalConditions() {
         val mockSpec = Mockito.mock(MediaQualitySpec::class.java)
         Mockito.`when`(mockSpec.imageFileSize).thenReturn(15)
-        val mediaList = MediaDownloadList(MediaContentType.GALLERY)
-        mediaList.addAll(mockGallery().take(25).toList())
-        mediaList.getMostSuitableMedia(mockSpec).forEach {
+        val mediaList = MediaDownloadInfo(GALLERY)
+        mediaList.mediaDownloadList.addAll(mockGallery().take(25).toList())
+        filterMediaInfo(mockSpec, mediaList).forEach {
             assertEquals(16, it.metadata.size)
         }
     }
@@ -85,9 +90,9 @@ class MediaDownloadListTest {
     fun getMostSuitableMedia_TestGallery_ThresholdBiggerThenMinImage() {
         val mockSpec = Mockito.mock(MediaQualitySpec::class.java)
         Mockito.`when`(mockSpec.imageFileSize).thenReturn(4)
-        val mediaList = MediaDownloadList(MediaContentType.GALLERY)
-        mediaList.addAll(mockGallery().take(25).toList())
-        mediaList.getMostSuitableMedia(mockSpec).forEach {
+        val mediaList = MediaDownloadInfo(GALLERY)
+        mediaList.mediaDownloadList.addAll(mockGallery().take(25).toList())
+        filterMediaInfo(mockSpec, mediaList).forEach {
             assertEquals(6, it.metadata.size)
         }
     }
