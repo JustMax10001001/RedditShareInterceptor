df932e6ccf217cbbe21daa5b84f3e2c19ad49e02
Max <rudko.max10@gmail.com>
2021-02-03 16:22:24 +0200

    Add media type for combined video and audio

--- app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaContentType.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/model/media/MediaContentType.kt
@@ -1,5 +1,5 @@
 package com.justsoft.redditshareinterceptor.model.media
 
 enum class MediaContentType {
-    IMAGE, VIDEO, GIF, TEXT, GALLERY, AUDIO
+    IMAGE, VIDEO, GIF, TEXT, GALLERY, AUDIO, VIDEO_AUDIO
 }
