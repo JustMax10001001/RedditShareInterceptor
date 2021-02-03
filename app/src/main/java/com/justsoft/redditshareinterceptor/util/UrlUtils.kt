68f77d347d2b1383bf9a53abafcc1c851d5761a4
Max <rudko.max10@gmail.com>
2021-02-03 13:26:50 +0200

    Rename UrlFeatures to UrlUtils and add URL-check function

--- app/src/main/java/com/justsoft/redditshareinterceptor/util/UrlUtils.kt
+++ app/src/main/java/com/justsoft/redditshareinterceptor/util/UrlUtils.kt
@@ -1,5 +1,6 @@
 package com.justsoft.redditshareinterceptor.util
 
+import android.util.Patterns
 import java.net.URLDecoder
 import java.net.URLEncoder
 
@@ -11,3 +12,8 @@ fun urlDecode(encodedUrl: String): String =
         .replace("&amp;", "&")
         .replace("&lt;", "<")
         .replace("&rt;", ">")
+
+fun checkUrlString(url: String?): Boolean {
+    url ?: return false
+    return Patterns.WEB_URL.matcher(url).matches()
+}
